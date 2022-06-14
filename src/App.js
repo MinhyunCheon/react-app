b3564112d5cc0e655d094930cc40722a66391c5a
Cheon <a02281127@gmail.com>
2022-06-15 00:00:59 +0900

    Deps

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
+  const [keyword, setKeyword] = useState(""); // useState의 초기값을 설정하지 않는 경우 경고 발생
   const onClick = () => setCounter(p => p + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   const runOnlyOnce = () => console.log("i run only once");
   useEffect(runOnlyOnce, []);
+  useEffect(() => { // keyword에 변화가 있을 때만 실행
+    console.log("Search for", keyword);
+  }, [keyword, counter]); // 배열 형태이기 때문에 여러 값 설정 가능
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here" />
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
     </div>
