0beff5425b551edc6290bd105f4103fda6497161
Cheon <a02281127@gmail.com>
2022-06-14 23:44:24 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const onClick = () => setCounter(p => p + 1);
-  console.log("call an api");
+  console.log("i run all the time");
+  const runOnlyOnce = () => console.log("i run only once");
+  useEffect(runOnlyOnce, []);
   return (
     <div>
       <h1>{counter}</h1>
