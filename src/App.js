59b29852a42d2c17a20e12db20b238e27db8d6f8
Cheon <a02281127@gmail.com>
2022-06-17 23:39:12 +0900

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("created");
+    return () => console.log("destroyed");
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setCounter] = useState(0);
-  const [keyword, setKeyword] = useState(""); // useState의 초기값을 설정하지 않는 경우 경고 발생
-  const onClick = () => setCounter(p => p + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
-  const runOnlyOnce = () => console.log("i run only once");
-  useEffect(runOnlyOnce, []); // dependency가 없는 경우, 최초 1회만 실행
-  useEffect(() => { // keyword에 변화가 있을 때만 실행
-    console.log("Search for", keyword);
-  }, [keyword, counter]); // 배열 형태이기 때문에 여러 값 설정 가능(둘 중 하나가 변경될 경우 실행)
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing(prev => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here" />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
