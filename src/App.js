d737f88a1b6b75e54f292cdacffbec20dd3b9af9
Cheon <a02281127@gmail.com>
2022-07-25 10:41:55 +0900

    Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if(toDo === "") return;
-    setToDos(arr => [toDo, ...toDos]);
-    setToDo("");
-  }
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then(response => response.json())
+      .then(json => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []); // 2번째 파라미터에 빈 배열을 넘기는 경우, 최초 1회만 실행
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <h1 />
-      <ul>
-        {/* index를 지정하지 않는 경우 경고 발생 */}
-        {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? <strong>Loading...</strong> :
+        <ul>
+          {coins.map(coin => 
+            <li>
+              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+            </li>
+          )}
+        </ul>}
+      
     </div>
   );
 }
