61d5ac522cbe12f04f0e45163bcaea037dbd2cfd
Cheon <a02281127@gmail.com>
2022-08-03 23:40:38 +0900

    cleanup

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,16 +5,10 @@ function Movie({medium_cover_image, title, summary, genres}) {
     return (
         <div>
             <img src={medium_cover_image} alt={title} />
-            <h2>
-                {/** <React.StrictMode>의 경우 Link로 url만 바뀌고 화면이 갱신되지 않음 */}
-                <Link to="/movie">{title}</Link>
-            </h2>
+            <h2><Link to="/movie">{title}</Link></h2>
             <p>{summary}</p>
             <ul>
-            {genres.map(g =>
-                <li key={g}>{g}</li>
-            )
-            }
+            {genres.map(g => <li key={g}>{g}</li>)}
             </ul>
         </div>
     );
