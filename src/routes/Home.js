daee829906378aeb5fb890e48ff667b0fa58164c
Cheon <a02281127@gmail.com>
2022-08-08 23:58:56 +0900

    Styles

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import styles from "./Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -19,9 +20,9 @@ function Home() {
   }, []);
   
   return (
-    <div>
+    <div className={styles.container}>
       {loading ? <h1>Loading...</h1> :
-        <div>
+        <div className={styles.movies}>
           {movies.map(movie =>
             <Movie
               key={movie.id}  // key는 파라미터로 넘길 수 없음
